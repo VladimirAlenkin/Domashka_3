fba289ab4ae648263b4f585579a315b5a4c95619
Vladimir <valenkin@yandex.ru>
2023-01-22 23:37:09 +0500

    task_2

---
+++ task_2/task_2.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Задача 2: подсчитать сумму цифр в числе");
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+int amount = 0;
+
+while ( N/10 >= 1 )
+{
+amount = amount + N%10;
+N = N/10;
+}
+amount = amount + N;
+Console.WriteLine("Cумма цифр в числе - "  + amount );
