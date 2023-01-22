8e4e6dd0389d27daf42aa56bc8b6c34c084c30c2
Vladimir <valenkin@yandex.ru>
2023-01-22 23:36:38 +0500

    task_1

---
+++ task_1/task_1.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задача 1: найти кубы чисел от 1 до N");
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+if (N < 1)
+{
+    Console.WriteLine("Введено некорректное число");
+}
+int count = 1;
+while (count <= N)
+{
+    int result = count*count*count;
+    Console.WriteLine("Число - " + count + "; куб числа - " + result);
+    count = count + 1;
+}
