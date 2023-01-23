e3fe890444678f9d9a833481b8167a52d1cfbc97
Vladimir <valenkin@yandex.ru>
2023-01-24 00:48:55 +0500

    task_4

---
+++ task_4/task_4.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задача 4: показать кубы чисел, заканчивающихся на четную цифру");
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число M, больше, чем N");
+int M = int.Parse(Console.ReadLine()!);
+int result = 1;
+int remains = 0;
+while ( N <= M )
+{
+result = N * N * N;
+remains = result % 2; 
+if (remains == 0 )
+{
+    Console.WriteLine("Число - "  + N + " в кубе - чётное: " + result);
+}
+N = N + 1;
+}
+
