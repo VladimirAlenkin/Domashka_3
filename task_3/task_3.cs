1cf3d8d24688ecd5095313eac28c5a4fe7b9ad41
Vladimir <valenkin@yandex.ru>
2023-01-24 00:25:40 +0500

    task_3

---
+++ task_3/task_3.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задача 3: написать программу вычисления произведения чисел от 1 до N");
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+int B = 1;
+for (int A = 2; A <= N; A++)
+{
+    B = B * A;
+}
+Console.WriteLine("Произведение чисел от 1 до "  + N +  " равно " + B );
+
